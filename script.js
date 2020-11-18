56d3022f1f0c7a98867063ebf15c8c6345e40d3c
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-18 13:43:27 -0500

    First step

---
+++ script.js
@@ -0,0 +1,18 @@
+// let fArray=[32, 33.8, 35.6]
+
+// for (let fArray=[],i=0;i<=100;++i) a[i]=i;
+
+
+
+for (let celcius=0;celcius<=100;++celcius){
+    console.log(celcius);
+}
+
+
+
+while (celcius<=100) {let farenheight = ((celcius*9/5) +32);
+console.log(farenheight)}
+
+
+
+// (0°C × 9/5) + 32 = 32°F
